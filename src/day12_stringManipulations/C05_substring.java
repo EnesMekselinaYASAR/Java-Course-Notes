9dc82e4ccceafbdff6f68f93ff8443dc9801a24b
Enes Mekselina Yaşar <av.enesmyasar@gmail.com>
2023-03-22 13:12:01 +0300

    22.03.2023

---
+++ src/day12_stringManipulations/C05_substring.java
@@ -0,0 +1,24 @@
+package day12_stringManipulations;
+
+import java.util.Scanner;
+
+public class C05_substring {
+    public static void main(String[] args) {
+
+         /*
+        kullanicidan 4 harfli bir kelime isteyin ve giriilen kelimeyi tersten yazdirin
+         */
+
+        String input="Veli";
+
+        String tersInput=input.substring(3)+
+                         input.substring(2,3)+
+                         input.substring(1,2)+
+                         input.substring(0,1);
+
+        System.out.println(tersInput);
+
+
+
+    }
+}
