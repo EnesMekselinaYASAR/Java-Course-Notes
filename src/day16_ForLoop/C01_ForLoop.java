c30601db17c6f5b2d719792b50a8585e70f7b9a0
Enes Mekselina Yaşar <av.enesmyasar@gmail.com>
2023-03-25 16:41:46 +0300

    25.03.2023

---
+++ src/day16_ForLoop/C01_ForLoop.java
@@ -0,0 +1,19 @@
+package day16_ForLoop;
+
+public class C01_ForLoop {
+    public static void main(String[] args) {
+         //Verilen bir pozitif tam sayinin pozitif tam bolenlerini yazdiralim.
+
+        int input=20;
+
+        for (int i = 1; i <=input ; i++) {
+            if (input%i==0){
+                System.out.println(i+" ");
+            }
+
+        }
+
+
+
+    }
+}
