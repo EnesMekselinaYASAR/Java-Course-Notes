40a392bb1895175180bd9b94bf8cd1ce65e7bd18
Enes Mekselina Yaşar <av.enesmyasar@gmail.com>
2023-03-27 20:45:33 +0300

    27.03.2023

--- src/day19_Scope/C02_StaticVariables.java
+++ src/day19_Scope/C02_StaticVariables.java
@@ -2,12 +2,55 @@ package day19_Scope;
 
 public class C02_StaticVariables {
 
+    static int staticSayi=10;
+    String isim="Mehmet";
 
-
-
+    static int degersizStaticVar;
+    int degersizInstanceVariable;
 
     /*
      Class level'daki variable'lara deger atamasak da Java kabul ediyor.
      */
+
+    public static void main(String[] args) {
+         /*
+        bir variable static olusturulduysa
+        objeler icin degil class icin gecerlidir.
+         */
+        System.out.println(staticSayi); // 10
+        staticMethod();
+        staticSayi=12;
+        System.out.println(staticSayi); //12
+        C02_StaticVariables obje1=new C02_StaticVariables();
+        obje1.staticOlmayanMethod();
+        System.out.println(staticSayi); //
+
+        /*
+        Instance bir variable degerini bulmak icin objenin olusturulmasindan itibaren
+        istenen satira kadar kodu takip etmeliyiz.
+        Static variable'da ustte class'in en basindan baslayarak
+        istenen satira kadar kodu takip edip
+        static variable'in son degerini bulmamiz gerekir.
+         */
+
+    }
+
+    public static void staticMethod(){
+        System.out.println(staticSayi); // 10
+
+    }
+
+    public void staticOlmayanMethod(){
+         /*
+         Static variable'lar class icerisinden her yerden ulasılabilir.
+         Static olsun veya olmasin tum method'lar static variable'lari gorebilir ve degistirebilirler.
+         farkli methodlarda static variable'in hangi degeri alacagini bilmek istiyorsak
+         class'in basindan itibaren kodun calismasini takip etmeliyiz.
+         Method ne zaman cagrilirsa o anki static variable degerini method'da kullanabiliriz
+         */
+        System.out.println(staticSayi); // 12
+        staticSayi=20;
+
+    }
 
 }
