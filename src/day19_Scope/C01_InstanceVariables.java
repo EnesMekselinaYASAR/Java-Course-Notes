74f3753839a5696b004e4f5d9079b51d16b9f709
Enes Mekselina Yaşar <av.enesmyasar@gmail.com>
2023-03-27 20:00:01 +0300

    27.03.2023

---
+++ src/day19_Scope/C01_InstanceVariables.java
@@ -0,0 +1,70 @@
+package day19_Scope;
+
+public class C01_InstanceVariables {
+    /*
+            class icerisinde her yerden kullanmak istedigimiz variable'ları
+            class level'da (Class'in icinde ama method'larin dışında)
+            genellikle class level variable'lar class'in basinda olusturulur.(sart degil)
+            class level'daki variable'lar icin 2 scope vardir.
+                1- static variables (Class variable'lari da denir.)
+                2- instance (static olmayan) variables (Obje variable'i da denir.)
+             */
+
+    int instSayi=20; // static olmayip class level'da oldugu icin instance variable'dir
+
+    public static void main(String[] args) {
+        int sayi=10;
+         /*System.out.println(instSayi);
+        instance variables static olmadigi icin static klup'e uye main method'dan direk
+        kullanamayiz.
+        instance variables'larin diger adi object variable'dir.
+        dolayisiyla obje olusturursaniz instance variable'lari her yerden direk kullanabiliriz.
+            */
+
+        C01_InstanceVariables obje1=new C01_InstanceVariables();
+        System.out.println("obje1 deggismeden once: "+obje1.instSayi); // 20
+        obje1.instSayi=30;
+        System.out.println("obje1 degistikten sonra: "+obje1.instSayi); // 30
+        C01_InstanceVariables obje2=new C01_InstanceVariables();
+        System.out.println("obje2 degismeden once: "+obje2.instSayi); //
+        obje2.instSayi=25;
+        System.out.println("obje2 degistikten sonra: "+obje2.instSayi);
+        C01_InstanceVariables obje3=new C01_InstanceVariables();
+        System.out.println("obje3 degismeden once: "+obje3.instSayi);
+
+        /*
+        Her obje olustugunda instance (obje) variable'inin ilk atanan degerini alir.
+         */
+    }
+
+    public static void staticMethod() {
+        // System.out.println(sayi);
+
+        /*
+        Bir method'un icerisinde olusturulan variable,
+        sadece o method'un icinden kullanabilir.
+        (Local variable)
+         */
+        /*
+        instSayi=30;
+        Instance variable'lara main method dısındaki static method'lardan da
+        direkt ulasamayız.
+        Obje olusturursak ulasabiliriz.
+         */
+
+        C01_InstanceVariables obje4=new C01_InstanceVariables();
+        System.out.println(obje4.instSayi);
+
+    }
+
+    public void staticOlmayanMethod(){
+
+        System.out.println(instSayi);
+
+        /*
+        Instance variable'lar class icerisindeki
+        static olmayan method'lardan direkt kullanabilir.
+         */
+
+    }
+}
---
+++ src/day19_Scope/C02_StaticVariables.java
@@ -0,0 +1,13 @@
+package day19_Scope;
+
+public class C02_StaticVariables {
+
+
+
+
+
+    /*
+     Class level'daki variable'lara deger atamasak da Java kabul ediyor.
+     */
+
+}
