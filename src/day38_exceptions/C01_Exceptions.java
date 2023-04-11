159553d685de7acf05d07536b8e12adaef7eba49
Enes Mekselina Yaşar <av.enesmyasar@gmail.com>
2023-04-11 16:42:31 +0300

    11.04.2023

---
+++ src/day38_exceptions/C01_Exceptions.java
@@ -0,0 +1,16 @@
+package day38_exceptions;
+
+public class C01_Exceptions {
+    public static void main(String[] args) {
+         /*
+        Bir sorunla karsilasmayi bekledigimiz noktalarda if-else ile sorunu yakalayip
+        onunla ilgili cözüm uretebiliriz.
+        Ama sorunu her zaman if-else ile cözemeyecegimizden Java try-catch block'lari
+        olusturmustur.
+         */
+
+
+
+
+    }
+}
