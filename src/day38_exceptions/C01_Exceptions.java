56a37bdf4fa48c2d1936e5ba11164a02ee9d5580
Enes Mekselina Yaşar <av.enesmyasar@gmail.com>
2023-04-11 16:56:58 +0300

    11.04.2023

--- src/day38_exceptions/C01_Exceptions.java
+++ src/day38_exceptions/C01_Exceptions.java
@@ -9,6 +9,24 @@ public class C01_Exceptions {
         olusturmustur.
          */
 
+        int a=1000;
+        int b=50;
+        int sayac=1;
+        while( sayac<100){
+            if (b==0){
+                System.out.println("islem yapilirken payda 0 oldu dikkat etmelisiniz");
+            }else {
+                System.out.println(a / b);
+            }
+
+            b--;
+            sayac++;
+        }
+
+
+
+
+
 
 
 
