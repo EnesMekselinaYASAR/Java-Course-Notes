41bec1abef7f2e9467c7c736b175f3cc4eb46064
Enes Mekselina Yaşar <av.enesmyasar@gmail.com>
2023-04-12 18:10:45 +0300

    12.04.2023

--- src/day40_final/DChildOfAbstract.java
+++ src/day40_final/DChildOfAbstract.java
@@ -1,4 +1,33 @@
 package day40_final;
 
 public class DChildOfAbstract extends CAbstract{
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+     /*
+        Abstract class'lar child class'larin sahip olmalari gereken
+        mecburi ozellikleri belirler.
+        (Gunluk hayatımızdaki standardlar gibi)
+        Abstract bir class'i parent edinen tum child'lar parent abstract class'daki
+        tum abstract method'lari override etmek zorundadirlar.
+         */
+
+
+
+
+
+
+
 }
