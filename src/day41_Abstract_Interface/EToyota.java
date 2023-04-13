9719efe095b303f60e764b94bb4b5ca107069706
Enes Mekselina Yaşar <av.enesmyasar@gmail.com>
2023-04-13 12:05:48 +0300

    13.04.2023

---
+++ src/day41_Abstract_Interface/EToyota.java
@@ -0,0 +1,18 @@
+package day41_Abstract_Interface;
+
+public abstract class EToyota extends DAraba{
+
+    @Override
+    protected void motor() {
+        System.out.println("Toyota cevreci motor kullanir");
+    }
+    /*
+    Parent class'daki standart belirleyici method'lar (abstract method)'larin tamami
+    child class tarafindan override edilmelidir.
+
+    concrete method'larin override edilme mecburiyeti yoktur
+    istersek override edebiliriz, istemezsek etmeyiz.
+    Aslinda toyota class'i da obje uretecegimiz bir class degil bu durumda eger proje
+    tasarimi yapiyorsaniz Toyota class'ini da abstract yapmamiz guzel olur.
+     */
+}
