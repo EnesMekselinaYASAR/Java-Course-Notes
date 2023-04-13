542851edf2d1e58864f1817bce8244bd15a4bbed
Enes Mekselina Yaşar <av.enesmyasar@gmail.com>
2023-04-13 10:46:42 +0300

    12.04.2023

--- src/day40_final/CAbstract.java
+++ src/day40_final/CAbstract.java
@@ -3,24 +3,15 @@ package day40_final;
 public abstract class CAbstract {
 
     void toplama(){
-        System.out.println("Bu method toplama yapar");
+        System.out.println("Bu method toplama yapar");}
+
+    abstract void carpma();
+
+    abstract void bolme();
+
+    void cikarma(){
+        System.out.println("Bu method cikarma yapar");
     }
-
-    //void carpma();
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
       /*
     Bir method'un basina abstract yazarsaniz bu method'un child class'lar icin bir
