89ee66d3241811b83a33acc5f05c07630a5f8a7c
Enes Mekselina Yaşar <av.enesmyasar@gmail.com>
2023-04-15 10:49:39 +0300

    15.04.2023

---
+++ src/day45_Collections/C01_Set.java
@@ -0,0 +1,13 @@
+package day45_Collections;
+
+public class C01_Set {
+    public static void main(String[] args) {
+         /*
+        Verilen bir array'deki tekrar eden elementleri silip tekrarsiz
+        halini array'e atayan kod yaziniz.
+         */
+        int arr[] = {4, 5, 3, 6, 8, 5, 1, 9, 0, 4, 2, 5, 7, 9, 1, 2, 5, 7, 6};
+
+
+    }
+}
---
+++ src/day45_Collections/C02_CollectionsDataTuru.java
@@ -0,0 +1,13 @@
+package day45_Collections;
+
+public class C02_CollectionsDataTuru {
+    public static void main(String[] args) {
+         /*
+        Collections <DataTuru>'nu Object secmeniz durumunda Collection'a farkli
+        data turlerinden objeler koymamiza izin verir.
+        Bu depolama acisindan bize esneklik saglar.
+        Ancak islem yaparken tum ıbjeleri Object olarak tanimladigimizdan cok fazla
+        casting yapmamiz gerekebilir.
+         */
+    }
+}
