50e7bbdeda7238af01cd4ae26d45e60f7e643fa5
Enes Mekselina Yaşar <av.enesmyasar@gmail.com>
2023-04-15 11:32:33 +0300

    15.04.2023

--- src/day45_Collections/C01_Set.java
+++ src/day45_Collections/C01_Set.java
@@ -1,5 +1,9 @@
 package day45_Collections;
 
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
 public class C01_Set {
     public static void main(String[] args) {
          /*
@@ -7,6 +11,29 @@ public class C01_Set {
         halini array'e atayan kod yaziniz.
          */
         int arr[] = {4, 5, 3, 6, 8, 5, 1, 9, 0, 4, 2, 5, 7, 9, 1, 2, 5, 7, 6};
+        Set<Integer> tekrarsizSet= new HashSet<>();
+
+        for (int each: arr
+             ) {
+            tekrarsizSet.add(each);
+        }
+
+        System.out.println("set : "+tekrarsizSet); // [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+        int[] tekrarsizArr= new int[tekrarsizSet.size()];
+
+        System.out.println(Arrays.toString(tekrarsizArr)); // [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+
+        int i=0;
+        for (Integer each:tekrarsizSet
+             ) {
+            tekrarsizArr[i]=each;
+            i++;
+        }
+        System.out.println(Arrays.toString(tekrarsizArr)); // [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+        arr=tekrarsizArr;
+        System.out.println("array in son hali"+Arrays.toString(arr)); // // [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
 
 
     }
