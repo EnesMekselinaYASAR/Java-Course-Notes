297496164b324412c5366ee03dbaf40e35bdab16
Enes Mekselina Yaşar <av.enesmyasar@gmail.com>
2023-04-16 11:44:19 +0300

    16.04.2023

---
+++ src/day47_Maps/C04_Entry.java
@@ -0,0 +1,16 @@
+package day47_Maps;
+
+public class C04_Entry {
+    public static void main(String[] args) {
+        /*
+        Java Entry <K, V> map'in icerisinde bulunan her bir koydi
+        K=V seklinde tutar.
+        dolayisiyla herhangi bir Entry'e ulastigimizda hem key'e hem de
+        value'ya ulasabilir ve istedigimiz islemleri yapabiliriz
+        entry.getKey() bize key'i getirir
+        entry.getValue() bize value'yu getirir
+        entry.setValue() value'yu istedigimiz deger olarak update eder
+         */
+
+    }
+}
